b424abdaaadd51fb2e332c94360af6a310277954
Charlotte <charlottefries@dhcp-18-111-65-117.dyn.MIT.EDU>
2018-07-24 10:50:24 -0400

    updated new_bank_code with proper packing

--- test_codes/new_bank_code.py
+++ test_codes/new_bank_code.py
@@ -4,6 +4,7 @@ from Crypto.PublicKey import RSA
 from Crypto import Random
 from Crypto.Hash import SHA256
 from databases import DB
+import struct
 
 # don't forget!!!! using AES mode CTR
 
@@ -28,10 +29,14 @@ print transaction_AES_key
 ATM_keys = RSA.generate(2048, random_generator)
 ATM_public_key = ATM_keys.publickey()
 
-bank_package_1 = transaction_id + ", " + transaction_AES_key
+bank_package_1 = struct.pack(">32s128s", transaction_id, transaction_AES_key)
 bank_package_1_encrypted = ATM_public_key.encrypt(bank_package_1, ATM_public_key)
 bank_package_1_hashed = SHA256.new(bank_package_1).hexdigest()
 
 print transactions.get_key(transaction_id)
 print bank_package_1_encrypted
 print bank_package_1_hashed
+
+decrypted = ATM_keys.decrypt(bank_package_1_encrypted)
+unpacked = struct.unpack(">32s128s", decrypted)
+print unpacked
